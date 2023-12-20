6e1ff28b5413672f336ec585ab87f8145ab12553
Skyrooow <151015957+Skyrooow@users.noreply.github.com>
2023-12-20 18:30:40 +0100

    Main panel (parent)

---
+++ panels/PT_nr_cleanup.py
@@ -0,0 +1,15 @@
+import bpy
+
+from . import NRCleanupPanel
+
+
+class VIEW3D_PT_nr_cleanup(NRCleanupPanel, bpy.types.Panel):
+    bl_label = "Ninja Ripped Map Cleanup"
+
+    def draw(self, context):
+        layout = self.layout
+
+
+
+
+
