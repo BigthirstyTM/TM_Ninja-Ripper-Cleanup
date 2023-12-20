6831d86a639315dd5c11ae248501c4cc78f13c31
Skyrooow <151015957+Skyrooow@users.noreply.github.com>
2023-12-20 18:31:03 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,25 +16,31 @@ bl_info = {
 
 
 # Import props
-
+from .properties.nr_cleanup_props import NRCleanup_Props
 # Import operators
 from .operators.OT_select_collection_from_nr import COLLECTION_OT_select_collection_from_nr
 from .operators.OT_make_route_collection import OBJECT_OT_make_route_collection
-from .operators.OT_remove_vertical_faces import MESH_OT_delete_vertical_faces
+from .operators.OT_delete_vertical_faces import MESH_OT_delete_vertical_faces
 # Import panels
-from .panels.PT_select_collection_from_rip import VIEW3D_PT_nr_cleanup
+from .panels.PT_nr_cleanup import VIEW3D_PT_nr_cleanup
+from .panels.PT_select_collection_from_nr import VIEW3D_PT_select_collection_from_nr
+from .panels.PT_make_route_collection import VIEW3D_PT_make_route_collection
+from .panels.PT_delete_vertical_faces import VIEW3D_PT_delete_vertical_faces
 
 
 # Define register order
 classes = (
     # Props
-
+    NRCleanup_Props,
     # Operators
     COLLECTION_OT_select_collection_from_nr,
     OBJECT_OT_make_route_collection,
     MESH_OT_delete_vertical_faces,
-    # Panels
+    # Panels (order matters here)
     VIEW3D_PT_nr_cleanup,
+    VIEW3D_PT_select_collection_from_nr,
+    VIEW3D_PT_make_route_collection,
+    VIEW3D_PT_delete_vertical_faces,
 )
 
 
@@ -43,10 +49,12 @@ def register():
     for cls in classes:
         bpy.utils.register_class(cls)
 
+    bpy.types.Scene.nr_cleanup_props = bpy.props.PointerProperty(type=NRCleanup_Props)
+
 # Unregister addon
 def unregister():
     for cls in reversed(classes):
-        bpy.utils.register_class(cls)
+        bpy.utils.unregister_class(cls)
     
 
 if __name__ == "__main__":
