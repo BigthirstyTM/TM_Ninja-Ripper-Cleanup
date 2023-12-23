213a7021fade3925397e5d43d0c5bf7f45bb9ef7
Skyrooow <151015957+Skyrooow@users.noreply.github.com>
2023-12-23 17:20:17 +0100

    Better UI

--- panels/PT_clean_imported_nr.py
+++ panels/PT_clean_imported_nr.py
@@ -1,20 +1,37 @@
 import bpy
 
 from .panels import NRCleanupChildPanel
-from ..operators.OT_select_collection_from_nr import COLLECTION_OT_select_collection_from_nr
-from ..operators.OT_make_route_collection import OBJECT_OT_make_route_collection
+
+from ..operators import (
+    WINDOW_OT_message_popup,
+    COLLECTION_OT_select_collection_from_nr,
+    OBJECT_OT_make_route_collection,
+)
+
 
 class VIEW3D_PT_clean_imported_nr(NRCleanupChildPanel, bpy.types.Panel):
-    bl_label = "Clean Imported NR"
+    bl_label = 'Clean Imported NR'
+    bl_options = {'HEADER_LAYOUT_EXPAND'}
 
     def draw(self, context):
-        layout = self.layout
-
         nrc_props = bpy.context.scene.nrc_props
-
-        col = layout.column()
-        col.prop(nrc_props, "nr_collection", text="Map collection")
-        col.operator(COLLECTION_OT_select_collection_from_nr.bl_idname, text="01. Select Map Collection")
-
-        col = layout.column()
-        col.operator(OBJECT_OT_make_route_collection.bl_idname, text="02. Create Route Collection")
+        layout = self.layout
+        # Select map collection
+        box = layout.box()
+        row = box.row()
+        row.label(text='01. Select Map Collection')
+        op = row.operator(WINDOW_OT_message_popup.bl_idname, text='', icon='QUESTION')
+        op.message = \
+            'hey is this working ?\n' \
+            + 'OK FINE'   
+        box.prop(nrc_props, 'nr_collection', text='')
+        box.operator(COLLECTION_OT_select_collection_from_nr.bl_idname, text='Select Map Collection')
+        # Create route collection
+        box = layout.box()
+        row = box.row()
+        row.label(text='02. Create Route Collection')
+        op = row.operator('WINDOW_OT_message_box', text='', icon='QUESTION')
+        op.message = \
+            'hey is this working ?\n' \
+            + 'OK FINE'   
+        box.operator(OBJECT_OT_make_route_collection.bl_idname, text='Create Route Collection')
--- panels/PT_cleaning_tools.py
+++ panels/PT_cleaning_tools.py
@@ -1,16 +1,37 @@
 import bpy
 
 from .panels import NRCleanupChildPanel
-from ..operators.OT_delete_vertical_faces import MESH_OT_delete_vertical_faces
-from ..operators.OT_delete_by_materials import MESH_OT_delete_by_materials
+
+from ..operators import (
+    WINDOW_OT_message_popup,
+    MESH_OT_delete_vertical_faces,
+    MESH_OT_delete_by_materials,
+    MESH_OT_delete_not_linked_flat_faces,
+)
 
 
 class VIEW3D_PT_cleaning_tools(NRCleanupChildPanel, bpy.types.Panel):
-    bl_label = "Cleaning Tools"
+    bl_label = 'Cleaning Tools'
+    bl_options = {'HEADER_LAYOUT_EXPAND'}
 
     def draw(self, context):
         layout = self.layout
-
         col = layout.column()
-        col.operator(MESH_OT_delete_vertical_faces.bl_idname, text="Delete vertical faces")
-        col.operator(MESH_OT_delete_by_materials.bl_idname, text="Delete faces by material")
+        row = col.row()
+        if context.mode == 'OBJECT':
+            row.label(text='Object mode', icon='OBJECT_DATAMODE')
+            op = row.operator(WINDOW_OT_message_popup.bl_idname, text='', icon='QUESTION')
+            op.message = \
+                'hey is this working ?\n' \
+                + 'OK FINE'
+            col.operator(MESH_OT_delete_vertical_faces.bl_idname, text="Delete vertical faces")
+            col.operator(MESH_OT_delete_by_materials.bl_idname, text="Delete faces by material")
+        elif context.mode == 'EDIT_MESH':
+            row.label(text='Edit mode', icon='EDITMODE_HLT')
+            op = row.operator(WINDOW_OT_message_popup.bl_idname, text='', icon='QUESTION')
+            op.message = \
+                'hey is this working ?\n' \
+                + 'OK FINE'
+            col.operator(MESH_OT_delete_not_linked_flat_faces.bl_idname, text="Delete not linked flat faces")
+        else:
+            row.label(text='Go in object or edit mode', icon='QUESTION')
