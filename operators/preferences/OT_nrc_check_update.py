35bffc02983979ea6d22c0b18312fe3b5520654a
Skyrooow <151015957+Skyrooow@users.noreply.github.com>
2023-12-26 10:00:53 +0100

    Update OT_nrc_check_update.py

--- operators/preferences/OT_nrc_check_update.py
+++ operators/preferences/OT_nrc_check_update.py
@@ -1,4 +1,5 @@
 import bpy
+import threading
 
 from ...utils.update import AddonUpdate
 
@@ -11,7 +12,9 @@ class PREFERENCES_OT_nrc_check_update(bpy.types.Operator):
         AddonUpdate.check_for_new_release()
 
         if AddonUpdate.can_update:
-            self.report({'INFO'}, 'NR_Cleanup update available !')
+            self.report({'INFO'}, f'NR_Cleanup  update available !')
+        elif AddonUpdate.new_addon_available and not AddonUpdate.current_blender_supported:
+            self.report({'ERROR'},f'A new version is available but blender version is too old ! Minimal is {AddonUpdate.latest_minimal_blender_version}).')
         else:
             self.report({'INFO'}, 'No update available.')
 
