e05bd29f29dc7666c1839716ba57644ec6c5097d
Skyrooow <151015957+Skyrooow@users.noreply.github.com>
2023-12-26 10:01:00 +0100

    Update OT_nrc_do_update.py

--- operators/preferences/OT_nrc_do_update.py
+++ operators/preferences/OT_nrc_do_update.py
@@ -12,6 +12,7 @@ class PREFERENCES_OT_nrc_do_update(bpy.types.Operator):
         return bpy.types.WindowManager.invoke_props_dialog(self)
 
     def execute(self, context):
+        AddonUpdate.do_update()
         return {'FINISHED'}        
 
     def draw(self, context):
