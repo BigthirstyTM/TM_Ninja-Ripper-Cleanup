4767e36ada27119a9cdbb727cb3a4903277ed3f7
Skyrooow <151015957+Skyrooow@users.noreply.github.com>
2023-12-27 08:53:25 +0100

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,3 @@
+# Github URLs
+GIT_REPO_URL = 'https://github.com/BigthirstyTM/TM_Ninja-Ripper-Cleanup.git'
+URL_RELEASES = "https://api.github.com/repos/BigthirstyTM/TM_Ninja-Ripper-Cleanup/releases"
