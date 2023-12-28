cc91aeee4c31480ab7d5ee87b9beececde01e904
Skyrooow <151015957+Skyrooow@users.noreply.github.com>
2023-12-28 16:39:08 +0100

    Working 'delete not linked flat faces' operator

--- operators/cleaning_tools/mesh/OT_delete_not_linked_flat_faces.py
+++ operators/cleaning_tools/mesh/OT_delete_not_linked_flat_faces.py
@@ -1,7 +1,6 @@
 import bpy
 import bmesh
-from mathutils import Vector
-from math import radians, sin
+from math import radians
 
 
 class MESH_OT_delete_not_linked_flat_faces(bpy.types.Operator):
@@ -10,15 +9,16 @@ class MESH_OT_delete_not_linked_flat_faces(bpy.types.Operator):
     bl_label = 'Delete Not Linked Flat Faces'
     bl_options = {'REGISTER', 'UNDO'}
     
-    max_flat_angle: bpy.props.FloatProperty(
-        name='Maximum angle',
+    sharpness: bpy.props.FloatProperty(
+        name='Sharpness',
         min=radians(0.0),
-        max=radians(89.0),
+        max=radians(179.0),
         step=10,
         default=radians(1.0),
         precision=3,
         subtype='ANGLE',
         unit='ROTATION',
+        description='Maximum angle to consider as flat',
     )
     
     @classmethod
@@ -33,24 +33,48 @@ class MESH_OT_delete_not_linked_flat_faces(bpy.types.Operator):
         return context.window_manager.invoke_props_dialog(self, width=300)
     
     def execute(self, context):
-        for obj in context.objects_in_mode:
-            # Get the mesh data
+        objects = context.objects_in_mode
+
+        # Extend selection to linked flat faces
+        bpy.ops.mesh.faces_select_linked_flat(sharpness=self.sharpness)
+        for obj in objects:
+            obj.update_from_editmode()
+        linked_flat_per_obj = [[f.index for f in obj.data.polygons if f.select] for obj in objects]
+
+        # Extend selection to linked faces
+        bpy.ops.mesh.select_linked()
+        for obj in objects:
+            obj.update_from_editmode()
+        
+
+        for i, obj in enumerate(objects):
+            # Get linked flat faces
+            linked_flat_faces = linked_flat_per_obj[i]
+
+            # Create bmesh
             mesh = obj.data
-            # Create a bew bmesh      
             bm = bmesh.from_edit_mesh(mesh)
+            bm.faces.ensure_lookup_table()
 
-            
-            selected_faces = [f for f in bm.faces if f.select]
+            # Deselect linked flat faces
+            for f in linked_flat_faces:
+                bm.faces[f].select = False
 
+            # List not linked flat faces
+            not_linked_flat_faces = [f for f in bm.faces if f.select]
+   
             # Delete not linked flat faces
-            bmesh.ops.delete(bm, geom=selected_faces, context='FACES')
+            bmesh.ops.delete(bm, geom=not_linked_flat_faces, context='FACES')
 
             # Write the bmesh back to the mesh
             bmesh.update_edit_mesh(mesh)
             bm.free()
-            self.report({'INFO'}, f'Deleted {len(selected_faces)} faces in {obj.name}')
+
+            self.report({'INFO'}, f'Deleted {len(not_linked_flat_faces)} faces in {obj.name}')
+
         return {'FINISHED'}
     
     def draw(self, context):
         layout = self.layout
-        layout.prop(self, 'max_flat_angle')
+        layout.label(text='Does not delete unconnected geometry.', icon='QUESTION')
+        layout.prop(self, 'sharpness')
